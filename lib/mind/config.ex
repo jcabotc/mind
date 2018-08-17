3a125f84d008319a6a14dd8aec0f634762b3bfb1
Jaime Cabot Campíns <jaime.cabot@cabify.com>
2018-08-17 21:43:56 +0200

    Mind.Config

---
+++ lib/mind/config.ex
@@ -0,0 +1,17 @@
+defmodule Mind.Config do
+  @type key_replicas :: pos_integer
+  @type read_quorum :: pos_integer
+  @type write_quorum :: pos_integer
+
+  @type t :: %__MODULE__{
+          key_replicas: key_replicas,
+          read_quorum: read_quorum,
+          write_quorum: write_quorum
+        }
+
+  defstruct [
+    :key_replicas,
+    :read_quorum,
+    :write_quorum
+  ]
+end
