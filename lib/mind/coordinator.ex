b0ac328cc41e1c575416a5b20372887c88475662
Jaime Cabot Campíns <jaime.cabot@cabify.com>
2018-08-19 17:37:45 +0200

    coordinator refactor: common logic extracted to a function

--- lib/mind/coordinator.ex
+++ lib/mind/coordinator.ex
@@ -2,40 +2,36 @@ defmodule Mind.Coordinator do
   alias Mind.{Config, Cluster, Store, Remote}
 
   def put(id, key, value) do
-    replicas = Config.Store.key_replicas(id)
+    mfa = {Store, :put, [id, key, value]}
     quorum = Config.Store.write_quorum(id)
 
-    snapshot = Cluster.snapshot(id, key, replicas)
-
-    request = %Remote.Request{
-      nodes: snapshot.nodes,
-      mfa: {Store, :put, [id, key, value]},
-      quorum: quorum,
-      timeout_ms: 5000
-    }
-
-    case Remote.run(id, request) do
+    case perform_request(id, key, mfa, quorum) do
       {:ok, _results} -> :ok
       {:timeout, _results} -> {:error, :timeout}
     end
   end
 
   def fetch(id, key) do
-    replicas = Config.Store.key_replicas(id)
+    mfa = {Store, :fetch, [id, key]}
     quorum = Config.Store.read_quorum(id)
 
+    case perform_request(id, key, mfa, quorum) do
+      {:ok, [result | _rest]} -> result
+      {:timeout, _results} -> {:error, :timeout}
+    end
+  end
+
+  defp perform_request(id, key, mfa, quorum) do
+    replicas = Config.Store.key_replicas(id)
     snapshot = Cluster.snapshot(id, key, replicas)
 
     request = %Remote.Request{
       nodes: snapshot.nodes,
-      mfa: {Store, :fetch, [id, key]},
+      mfa: mfa,
       quorum: quorum,
       timeout_ms: 5000
     }
 
-    case Remote.run(id, request) do
-      {:ok, [result | _rest]} -> result
-      {:timeout, _results} -> {:error, :timeout}
-    end
+    Remote.run(id, request)
   end
 end
