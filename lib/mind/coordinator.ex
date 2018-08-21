c5b597202aebeed04120cea9b4ebf7cf405eff0a
Jaime Cabot Campíns <jaime.cabot@cabify.com>
2018-08-21 18:35:51 +0200

    Coordinator refactor

--- lib/mind/coordinator.ex
+++ lib/mind/coordinator.ex
@@ -5,41 +5,42 @@ defmodule Mind.Coordinator do
   @timeout_ms 5000
 
   def put(id, key, value) do
-    replicas = Config.Store.key_replicas(id)
+    snapshot = get_snapshot(id, key)
     quorum = Config.Store.write_quorum(id)
-
     mfa = {Store, :put, [id, key, value]}
-    snapshot = Cluster.snapshot(id, key, replicas)
 
-    query = build_query(snapshot, mfa, quorum)
-
-    case Query.run(id, query) do
+    case run_query(id, snapshot, quorum, mfa) do
       {:ok, _results} -> :ok
-      {:timeout, _results} -> {:error, :timeout}
+      {:timeout, _partial_results} -> {:error, :timeout}
     end
   end
 
   def fetch(id, key) do
-    replicas = Config.Store.key_replicas(id)
+    snapshot = get_snapshot(id, key)
     quorum = Config.Store.read_quorum(id)
-
     mfa = {Store, :fetch, [id, key]}
-    snapshot = Cluster.snapshot(id, key, replicas)
 
-    query = build_query(snapshot, mfa, quorum)
-
-    case Query.run(id, query) do
+    case run_query(id, snapshot, quorum, mfa) do
       {:ok, [result | _rest]} -> result
-      {:timeout, _results} -> {:error, :timeout}
+      {:timeout, _partial_results} -> {:error, :timeout}
     end
   end
 
-  defp build_query(snapshot, mfa, quorum) do
-    %Query{
-      nodes: snapshot.nodes,
-      mfa: mfa,
-      quorum: quorum,
-      timeout_ms: @timeout_ms
-    }
+  defp get_snapshot(id, key) do
+    replicas = Config.Store.key_replicas(id)
+
+    Cluster.snapshot(id, key, replicas)
+  end
+
+  defp run_query(id, snapshot, quorum, mfa) do
+    query =
+      %Query{
+        nodes: snapshot.nodes,
+        mfa: mfa,
+        quorum: quorum,
+        timeout_ms: @timeout_ms
+      }
+
+    Query.run(id, query)
   end
 end
